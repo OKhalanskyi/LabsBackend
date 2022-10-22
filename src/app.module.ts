80e76b3efc1e047af8d73f8f7c3e7cc20aa94baa
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-22 16:48:46 +0300

    configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { CostModule } from './cost/cost.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UserModule, CategoryModule, CostModule],
+  imports: [ConfigModule.forRoot(),UserModule, CategoryModule, CostModule],
   controllers: [],
   providers: [],
 })
