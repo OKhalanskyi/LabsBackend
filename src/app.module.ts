5c8a873181c945d83d69676414a71a0905202243
Oleg Khalanskyi <okhalanskyi@gmail.com>
2022-10-22 15:16:44 +0300

    generated basic modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { CostModule } from './cost/cost.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CategoryModule, CostModule],
   controllers: [],
   providers: [],
 })
